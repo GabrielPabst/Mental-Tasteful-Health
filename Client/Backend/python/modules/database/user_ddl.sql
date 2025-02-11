d9668d2c8f07c9b9bfca4cafd5515c6faa2948bb
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2025-02-11 18:48:28 +0100

    Smol changes big outcome

--- Client/Backend/python/modules/database/user_ddl.sql
+++ Client/Backend/python/modules/database/user_ddl.sql
@@ -5,7 +5,9 @@ CREATE TABLE users(
     email varchar(320) NOT NULL,
     name varchar(255),
     profile_picture text,
+    nutriscore integer not null default 0,
     PRIMARY KEY(id)
 );
 CREATE UNIQUE INDEX users_google_id_key ON users USING btree ("google_id");
 CREATE UNIQUE INDEX users_email_key ON users USING btree ("email");
+
