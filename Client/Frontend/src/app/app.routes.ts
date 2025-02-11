67de293ab790a8f4edf7b81f5677b7f9b0067141
ClemensGugerbauer <c.gugerbauer@HTBLALeondin.onmicrosoft.com>
2025-02-11 09:22:42 +0100

    fixing routes

--- Client/Frontend/src/app/app.routes.ts
+++ Client/Frontend/src/app/app.routes.ts
@@ -4,8 +4,9 @@ import {LoginComponent} from './login/login.component';
 import {RefrigeratorComponent} from './refrigerator/refrigerator.component';
 
 export const routes: Routes = [
-  {path: 'recipes', pathMatch: "full", component: RecipesComponent},
-  {path: 'login', pathMatch: "full", component: LoginComponent},
+  {path: "recipes", pathMatch: "full", component: RecipesComponent},
+  {path: "login", pathMatch: "full", component: LoginComponent},
   {path: "refrigerator", pathMatch: "full", component: RefrigeratorComponent},
+  {path: "", pathMatch: "full", redirectTo: "/login"}
 ];
 
